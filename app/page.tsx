14875617ebb3f2250d8e4fc8fe1038513f554e4f
Mahammed-Siyabuddeen <mohd.sihab615@gmail.com>
2024-07-25 20:02:45 +0530

    CategoryList ui

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@ import Image from "next/image";
 import Header from "./components/Header";
 import Navigation from "./components/Navigation";
 import Slider from "./components/Slider";
+import CategoryList from "./components/CategoryList";
 export default function Home() {
   return (
     <div>
       <Header/>
       <Navigation/>
       <Slider/>
+      <CategoryList/>
     </div>
   );
 }
