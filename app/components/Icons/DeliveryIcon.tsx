6f1506b8c2bf304bc0859145205dc4992959dbba
Mahammed-Siyabuddeen <mohd.sihab615@gmail.com>
2024-07-26 20:45:38 +0530

    service ui

---
+++ app/components/Icons/DeliveryIcon.tsx
@@ -0,0 +1,7 @@
+import { SVGProps } from "react";
+
+export function DeliveryIcon(props: SVGProps<SVGSVGElement>) {
+    return (
+      <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}><path fill="currentColor" fillRule="evenodd" d="M2 2a1 1 0 0 0-1 1v14c0 1.354.897 2.498 2.129 2.872a3 3 0 0 0 5.7.128h6.341a3 3 0 0 0 5.7-.128A3.001 3.001 0 0 0 23 17v-4a5 5 0 0 0-5-5h-4V3a1 1 0 0 0-1-1zm13.171 16H14v-8h4a3 3 0 0 1 3 3v4a.997.997 0 0 1-.293.707a3 3 0 0 0-5.536.293m-9.878.293a1 1 0 1 1 1.414 1.414a1 1 0 0 1-1.414-1.414M17 19a1 1 0 1 1 2 0a1 1 0 0 1-2 0" clipRule="evenodd"></path></svg>
+    )
+  }
