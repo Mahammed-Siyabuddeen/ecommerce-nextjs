966d3b5a2e7b483ec5e7972f5661c2aa791456fe
Mahammed-Siyabuddeen <mohd.sihab615@gmail.com>
2024-07-27 12:36:49 +0530

    billing ui

--- app/billing/page.tsx
+++ app/billing/page.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Page = () => {
   return (
-    <div className="container mx-auto">
-       <div className="container ml-auto mr-auto w-2/6 my-12 flex flex-col gap-4">
+    <div className="container mx-auto ">
+       <div className="container ml-auto mr-auto w-2/6 my-12 flex flex-col gap-4 py-4 px-2 rounded-md   bg-yellow-400">
             <div>
                 <h1 className='text-3xl font-semibold'>Billing</h1>
                 <div className="flex justify-between p-3">
@@ -13,14 +13,14 @@ const Page = () => {
                 </div>
             </div>
             <div className="relative  justify-start gap-1 p-3 border rounded-md  font-medium flex items-center  shadow-sm">
-                <input type="radio" className='  outline-none   ' />
+                <input type="radio" name='method' className='  outline-none   ' />
                 <label htmlFor="">COD</label>
             </div>
             <div className="relative  justify-start gap-1 p-3 border rounded-md  font-medium flex items-center  shadow-sm">
-                <input type="radio" className='  outline-none   ' />
+                <input type="radio" name='method' className='  outline-none   ' />
                 <label htmlFor="">Online Payment</label>
             </div>
-            <button  className="text-center bg-yellow-400  p-3 rounded-md ">Order</button>
+            <button  className="text-center bg-white  p-3 rounded-md ">Order</button>
 
             
         </div>
