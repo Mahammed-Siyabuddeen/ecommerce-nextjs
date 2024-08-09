3d580df0e272baab4bae51f4415bcc6a3a1fd44f
Mahammed-Siyabuddeen <mohd.sihab615@gmail.com>
2024-08-09 20:55:54 +0530

    google login components

---
+++ components/GoogleLogin.tsx
@@ -0,0 +1,47 @@
+import { googleAuth } from '@/Services/googleAuth.services';
+import { setUser } from '@/features/authSlice';
+import { AppDispatch, RootState } from '@/features/redux/store';
+import { CredentialResponse,GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google'
+import { useRouter } from 'next/navigation';
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+
+const GoogleLoginComponent = () => {
+    const Dispatch = useDispatch<AppDispatch>()
+    const router=useRouter()
+    const user = useSelector((state: RootState) => state.user)
+
+    const handleAuthSuccess = (credentialResponse: CredentialResponse) => {
+        console.log(credentialResponse);
+        if (!credentialResponse.clientId || !credentialResponse.credential)
+            return
+
+        googleAuth(
+            {
+                data: { clientId: credentialResponse.clientId, credential: credentialResponse.credential }
+            }
+        ).then(({data})=>{
+                console.log(data);
+                Dispatch(setUser({...data}));
+                router.push('/');
+                
+        }).catch((err)=>{
+            console.log(err);
+            
+        })
+
+
+    }
+  return (
+    <GoogleOAuthProvider  clientId='734516501673-tmvvlb23d5levqadblop6fq76tjq0vt4.apps.googleusercontent.com'>
+    <GoogleLogin 
+        onSuccess={(credentialResponse: CredentialResponse) => handleAuthSuccess(credentialResponse)}
+        onError={() => console.log('error in google auth')}
+    >
+
+    </GoogleLogin>
+</GoogleOAuthProvider>
+  )
+}
+
+export default GoogleLoginComponent
