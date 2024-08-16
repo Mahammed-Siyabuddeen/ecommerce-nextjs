3b18dafe85517555ce2946fc7a857703130711aa
mahamme-siyabuddeen <mohd.sihab615@gmail.com>
2024-08-16 07:18:31 +0530

    addressType,OrderItem code change

--- components/OrderItem.tsx
+++ components/OrderItem.tsx
@@ -1,7 +1,12 @@
 import React, { FC } from 'react'
 import { allOrderType } from './Types/allOrderType'
 
-const OrderItem: FC = ({ order }: { order: allOrderType }) => {
+interface prop {
+  order: allOrderType,
+  setIsopen: React.Dispatch<React.SetStateAction<boolean>>,
+  setCurrentOrder: React.Dispatch<React.SetStateAction<allOrderType | undefined>>
+}
+const OrderItem: FC<prop> = ({ order, setIsopen, setCurrentOrder }) => {
   return (
     <tr className="hover:bg-slate-400 text-xs w-full relative">
       <td className=" truncate w-6 px-6 py-3" >{order._id}</td>
@@ -10,8 +15,13 @@ const OrderItem: FC = ({ order }: { order: allOrderType }) => {
       <td className=" truncate w-6 px-6 py-3" >{order.customer_email}</td>
       <td className=" truncate w-6 px-6 py-3" >{order.product_total}</td>
       <td className=" truncate w-6 px-6 py-3" >{order.product_quantity}</td>
-      <td className=" truncate w-6 px-6 py-3" >{ new Date(order.create_at).toLocaleDateString('en-US',{ day: '2-digit', month: '2-digit' })}</td>
-      <td className=" truncate w-6 px-6 py-3" >{order.status}</td>
+      <td className=" truncate w-6 px-6 py-3" >{new Date(order.create_at).toLocaleDateString('en-US', { day: '2-digit', month: '2-digit' })}</td>
+      <td onClick={() => {
+        setCurrentOrder(order)
+        setIsopen(true)
+      }}
+        className={`truncate w-6 px-6 py-3
+           ${order.status=='canceled'?'text-red-500':order.status=='Delivered'?'text-green-500':'text-yellow-400'} `} >{order.status}</td>
     </tr>
   )
 }
---
+++ components/Types/addressType.ts
@@ -0,0 +1,9 @@
+export interface addressType{
+    name:string,
+    street:string,
+    city:string,
+    state:string,
+    country:string,
+    zipcode:string,
+    phone:string,
+}
