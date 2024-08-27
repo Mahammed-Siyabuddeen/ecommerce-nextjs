6c73f1253b638d2307db14449b9ed446b62cf29f
mahamme-siyabuddeen <mohd.sihab615@gmail.com>
2024-08-27 10:38:37 +0530

    processing button

--- components/AddCategory.tsx
+++ components/AddCategory.tsx
@@ -1,27 +1,30 @@
 'use client'
 import { addCategory } from '@/Services/category.service';
-import { AxiosError } from 'axios';
 import React, { ChangeEvent, FormEvent, useState } from 'react'
 import ApiErrorResponse from '@/Services/ApiErrorResponse';
 import { useDispatch } from 'react-redux';
 import { AppDispatch } from '@/features/redux/store';
 import { appendCategory } from '@/features/categorySlice';
 import { toast } from 'sonner';
+import Processing from './Processing';
 
 
   
 const AddCategory = () => {
     const dispatch=useDispatch<AppDispatch>();
     const  [categoryName,setCategoryName]=useState<string>('')
+    const [loading,setLoading]=useState(false);
     const handleAddCategory = (e: FormEvent) => {
         e.preventDefault();
+        setLoading(true)
         addCategory({ name: categoryName}).then(({ data }) => {
             dispatch(appendCategory({...data,totalProduct:0}))
             toast.success('catergory successully aded')   
         }).catch((error) => {
-           ApiErrorResponse(error)
-        })
-
+            ApiErrorResponse(error)
+           
+        }).finally(()=>setLoading(false))
+       
     }
     return (
         <div className="w-2/4 p-6">
@@ -29,7 +32,11 @@ const AddCategory = () => {
             <form onSubmit={(e: FormEvent) => handleAddCategory(e)}>
                 <input required value={categoryName} 
                 onChange={(e:ChangeEvent<HTMLInputElement>)=>setCategoryName(e.target.value)} type="text" name='category' className="w-full border text-lg p-3 outline-none rounded" />
-                <button type='submit' className="bg-yellow-400 p-3 w-2/5 rounded m-3 text-white">Add</button>
+                {
+                loading?
+                <Processing/>
+                :<button type='submit' className="bg-yellow-400 p-3 w-2/5 rounded m-3 text-white">Add</button>
+                }
             </form>
         </div>
     )
